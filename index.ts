40935ff6567aa72800c82eea87cf024300f90985
Roberto Simonetti <robisim74@gmail.com>
2017-02-08 23:06:01 +0100

    Update description

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /**
  * Angular library starter.
- * Build a library compatible with Angular 2+, AoT compilation &amp; Tree shaking.
+ * Build a library compatible with Angular, AoT compilation & Tree shaking.
  * Written by Roberto Simonetti.
  * MIT license.
  * https://github.com/robisim74/angular-library-starter

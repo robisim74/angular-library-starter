2acd8a632716cfd188259488710aef015336c927
Roberto Simonetti <robisim74@gmail.com>
2017-11-02 21:32:59 +0100

    Upgrade to Angular v5 building process

--- public_api.ts
+++ public_api.ts
@@ -1,8 +1,8 @@
 /**
- * Angular library starter.
- * Build an Angular library compatible with AoT compilation & Tree shaking.
- * Written by Roberto Simonetti.
- * MIT license.
+ * Angular library starter
+ * Build an Angular library compatible with AoT compilation & Tree shaking
+ * Copyright Roberto Simonetti
+ * MIT license
  * https://github.com/robisim74/angular-library-starter
  */
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,23 @@
 import resolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 // Add here external dependencies that actually you use.
 const globals = {
     '@angular/core': 'ng.core',
     '@angular/common': 'ng.common',
     'rxjs/Observable': 'Rx',
-    'rxjs/Observer': 'Rx',
-    'rxjs/add/operator/map': 'Rx'
+    'rxjs/Observer': 'Rx'
 };
 
 export default {
-    entry: './dist/modules/angular-library-starter.es5.js',
-    dest: './dist/bundles/angular-library-starter.umd.js',
-    format: 'umd',
-    exports: 'named',
-    moduleName: 'ng.angularLibraryStarter',
-    plugins: [resolve()],
     external: Object.keys(globals),
-    globals: globals,
-    onwarn: () => { return }
+    plugins: [resolve(), sourcemaps()],
+    onwarn: () => { return },
+    output: {
+        format: 'umd',
+        name: 'ng.angularLibraryStarter',
+        globals: globals,
+        sourcemap: true,
+        exports: 'named'
+    }
 }
